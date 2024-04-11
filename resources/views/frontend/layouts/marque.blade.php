1033c703570a967c8a52aca938756830780bb0e9
lupenza <54394832+lupenza@users.noreply.github.com>
2024-04-11 17:36:54 +0300

    marque update

--- resources/views/frontend/layouts/marque.blade.php
+++ resources/views/frontend/layouts/marque.blade.php
@@ -4,14 +4,14 @@
             <div class="top-info-bar d-flex">
                 <div class="flex-item center">
                     <a href="#">
-                        <span> <i class="anm anm-worldwide"></i> Top niche Training Programs</span>
+                        <span> <i class="anm anm-worldwide"></i> TOP NICHE TRAINING PROGRAM</span>
                         <span> <i class="anm anm-truck-l"></i> SECURED PAYMENT</span>
                         <span> <i class="anm anm-redo-ar"></i> FREE RETURNS</span>
                     </a>
                 </div>
                 <div class="flex-item center">
                     <a href="#">
-                        <span> <i class="anm anm-worldwide"></i> Top niche Training Programs</span>
+                        <span> <i class="anm anm-worldwide"></i> TOP NICHE TRAINING PROGRAM</span>
                         <span> <i class="anm anm-truck-l"></i> SECURED PAYMENT</span>
                         <span> <i class="anm anm-redo-ar"></i> FREE RETURNS</span>
                     </a>
@@ -19,7 +19,7 @@
                 <div class="flex-item center">
                     <a href="#">
                         <span> <i class="anm anm-worldwide"></i> BUY ONLINE PICK UP IN STORE</span>
-                        <span> <i class="anm anm-truck-l"></i> FREE WORLDWIDE SHIPPING ON ALL ORDERS ABOVE $100</span>
+                        <span> <i class="anm anm-truck-l"></i> SECURED PAYMENT</span>
                         <span> <i class="anm anm-redo-ar"></i> FREE RETURNS</span>
                     </a>
                 </div>
